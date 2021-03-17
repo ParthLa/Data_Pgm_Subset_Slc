b3124e46eb836b554ea7f01bc89cfac12a2f68f7
harshading <harshad.ingole99@gmail.com>
2021-03-17 07:04:41 +0530

    fixed resources and unit testing

--- labeling/continuous_scoring/core.py
+++ labeling/continuous_scoring/core.py
@@ -6,16 +6,16 @@ class BaseContinuousScorer:
     def __init__(
         self,
         name: str,
-        f: Callable[..., int],
+        cf: Callable[..., int],
         resources: Optional[Mapping[str, Any]] = None,
         # pre: Optional[List[BasePreprocessor]] = None,
     ) -> None:
         self.name = name
-        self._f = f
+        self._cf = cf
         self._resources = resources or {}
     
-    def __call__(self, x: DataPoint) -> float:
-        return self._f(x,**self._resources)
+    def __call__(self, x: DataPoint, **kwargs) -> float:
+        return self._cf(x,**self._resources, **kwargs)
 
     def __repr__(self) -> str:
         return f"{type(self).__name__} {self.name}"
@@ -31,6 +31,6 @@ class continuous_scorer:
         self.name = name
         self.resources = resources
 
-    def __call__(self, f: Callable[..., int]) -> BaseContinuousScorer:
-        name = self.name or f.__name__
-        return BaseContinuousScorer(name=name, resources=self.resources, f=f)
+    def __call__(self, cf: Callable[..., int]) -> BaseContinuousScorer:
+        name = self.name or cf.__name__
+        return BaseContinuousScorer(name=name, resources=self.resources, cf=cf)
